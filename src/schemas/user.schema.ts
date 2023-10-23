a932605d074c276a2cdcaffbe90cd0f8499d6556
RamonCaBR <destroit.com@outlook.com>
2023-10-23 05:23:28 -0300

    add characters limit to userSchema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const userSchema = z.object({
   id: z.number(),
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
+  name: z.string().max(50),
+  email: z.string().email().max(50),
+  password: z.string().max(120),
   admin: z.boolean().optional(),
 });
 
